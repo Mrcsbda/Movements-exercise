cdf4e9520729f2dd2474a7a584e05523da8f7c8f
mrcsbda <macbedoya@gmail.com>
2023-06-09 10:03:09 -0500

    error

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 import getMovements from "./services/getMovimientos.js";
 import printMovements from "./modules/printMoviments.js";
+import { containerMovements } from "./services/data.js";
 
 document.addEventListener('DOMContentLoaded', async () => {
     const movements = await getMovements()
